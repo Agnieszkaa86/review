b498ea0b06668adce25dad33e6ef06eb2f5e81d4
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-02-20 23:04:28 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todos: [],
+      newTask: "",
+    };
+  }
+  handleInputChange = (event) => {
+    // console.log("value: ", event.target.value)
+    this.setState({ newTask: event.target.value });
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, { task: this.state.newTask, done: false }],
+      newTask: "",
+    })
+  }
+  // componentDidUpdate(prevProps,prevState) {
+  //   console.log("Component upadted:, State: ", this.state)
+  // }
+
+  render(){
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Add todo: 
+            <input type="text" onChange={this.handleInputChange} value={this.state.newTask} />
+          </label>
+          <button>Add</button>
+        </form>
+      </div>
+    )
+  }
+}
+export default App;
